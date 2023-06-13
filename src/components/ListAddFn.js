e5b0e553a6a68112b0cd00e4e8ec2914bc29c733
Fajar P M <fajar.pm@impulseglobal.net>
2023-06-14 00:07:36 +0300

    sync

--- src/components/ListAddFn.js
+++ src/components/ListAddFn.js
@@ -2,23 +2,17 @@ import React, { useState } from 'react'
 
  function ListAddFn() {
     const [lst, setLst] = useState([]);
-    const [lst2, setLst2] = useState([]);
     const [txtVal, setTxtVal] = useState("");
     const removeFromList = () => {
-        let lstC =lst;
+        let lstC = [...lst];
         lstC.pop();
         setLst(lstC)
-        const lstMap = lstC.map((item) => <h1>{item}</h1>)
-        // setLst2(lstMap)
     }
     const addToList = () => {
-        // let name = prompt('Enter the name')
         //let name = prompt('Enter the name')
-        let lstC = lst;
+        let lstC = [...lst];
         lstC.push(txtVal);
         setLst(lstC)
-        const lstMap = lstC.map((item) => <h1>{item}</h1>)
-        setLst2(lstMap)
     }
     const onChangeHandler = (event) => {
         let value = event.target.value;
